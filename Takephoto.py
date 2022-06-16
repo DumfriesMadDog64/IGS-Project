2c4ffc0122d3f35ce7604443f18758f359d0b597
DumfriesMadDog64 <Jonathancr1234@gmail.com>
2022-06-16 09:59:37 +0100

    Broken Code

--- Takephoto.py
+++ Takephoto.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 def takephoto():
     date = datetime.now().strftime("%Y_%m_%d-%I-%M-%S_%p")
-    camera = cv2.videocapture(0)
+    camera = cv2.VideoCapture(0)
     for i in range(1):
         return_value, image = camera.read()
         cv2.imwrite('opencv'+str(date)+'.png', image)
     del(camera)
     print("done")
 
-takephoto
+takephoto()
