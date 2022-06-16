588134a319c6b19b095e40dcf42ae73571f4b50f
WabbaMan <joejgilligan@gmail.com>
2022-06-16 09:46:30 +0100

    Fixed your code xx

--- Takephoto.py
+++ Takephoto.py
@@ -4,9 +4,8 @@ from datetime import datetime
 def takephoto():
     date = datetime.now().strftime("%Y_%m_%d-%I-%M-%S_%p")
     camera = cv2.videocapture(0)
-    for i in range(1):
-        return_value, image = camera.read()
-        cv2.imwrite('opencv'+str(date)+'.png', image)
+    return_value, image = camera.read()
+    cv2.imwrite('opencv'+str(date)+'.png', image)
     del(camera)
     print("done")
 
