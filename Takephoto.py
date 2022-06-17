dfec802546d3c5f5878da07538237a8f008c07ec
DumnfriesMadDog64 <Jonathancr1234@gmail.com>
2022-06-17 16:49:56 +0100

    asdf

--- Takephoto.py
+++ Takephoto.py
@@ -1,16 +1,13 @@
 import cv2
 from datetime import datetime
 
-from numpy import uint8
 
 def takephoto():
     date = datetime.now().strftime("%Y_%m_%d-%I-%M-%S_%p")
     camera = cv2.VideoCapture(0)
     return_value, image = camera.read()
     cv2.imwrite('opencv'+str(date)+'.png', image)
-    img = camera
     del(camera)
     print("Photo Done")
-    return img
 takephoto() 
 
